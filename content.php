dd5dc8fa9ebd27e2a64557a1cab0b62059de2914
Awadhesh-Pro <awadhesh31kumar@gmail.com>
2018-12-17 19:58:01 +0530

    add project-code

---
+++ content.php
@@ -0,0 +1,66 @@
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 localpadding mapimage">
+	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 content-padding">
+		<div class="about">
+			<h2 class="about-title contact-title text-center">About</h2>
+			<div class="about-grids">
+				<div class="about-info">
+					<h4 class="block-title">
+						<span style="margin-right: 0px;">
+							<i class="fa fa-sitemap"></i>&nbsp;&nbsp;
+							For the WeAreAlive family
+						</span>
+					</h4>
+					<p class="sub-title contact-subtitle p-mid-property text-justify" style="font-size:14px;">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						The motto of this website is simply to tell people about the ideas and policies made by the government of india for the indian citizens.
+						It doesn't inculcate the propogandas of the government, but to simply make people aware about the government moves for the people.
+						Onwards,26th may 2014 the government has come up with so many public beneficial policies which needs to be amplified 
+						Our moves (wia wearealive) is to make people know what they can do wia various government campaigns like <b class="br-proprty"> Digital India, Swachh Bharat Abhiyan, StartUp India, </b>etc.
+					</p>
+				</div>
+				
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rTzGMcOd8D0?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen>
+				</iframe>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-5 col-md-5 localpadding">
+			<div class="col-xs-4 col-sm-12 col-md-12 between-image">
+				<div class="about-imagearea bottom-data">
+					<div class="first-image">
+						<a href="img/background/bg2.jpg">
+							<img src="img/background/bg2.jpg" class="img-responsive"/>
+						</a>   
+					</div>
+					<p class="first hidden-xs">President House</p>
+				</div>
+			</div>
+			<div class="col-xs-4 col-sm-6 col-md-6 between-image">
+				<div class="about-imagearea bottom-data">
+					<div class="first-image">
+						<a href="img/background/bg1.jpg">
+							<img src="img/background/bg1.jpg" class="img-responsive"/>
+						</a>
+					</div>
+					<p class="second hidden-xs">Festival</p>
+				</div>
+			</div>
+			<div class="col-xs-4 col-sm-6 col-md-6 between-image">
+				<div class="about-imagearea bottom-data">
+					<div class="first-image">
+						<a href="#">
+							<img src="img/background/bg2.jpg" class="img-responsive"/>
+						</a>
+					</div>
+					<p class="three hidden-xs">India Gate</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	</div>
+</div>
+			 
---
+++ css-in header.php
@@ -0,0 +1,22 @@
+<style>
+.navbar-inverse 
+{
+	background-color: #16161D;
+}
+.navbar-inverse .navbar-nav > li > a 
+{
+	color: #FFF;
+}
+	.between-image
+{
+	padding: 0px 5px 15px 5px;
+}
+	.content-padding
+{
+	padding: 0px;
+}
+	.dropdown-menu > li > a:hover 
+{
+	background-color: #000;
+}
+</style>
